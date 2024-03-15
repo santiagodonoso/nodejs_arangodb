5f0d41d9301125d1bc6c3d4e980adcac49dcfd12
Santiago Donoso <santiagodonoso@gmail.com>
2024-03-15 12:53:06 +0100

    ...

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
+const http = require('http');
 // const PORT = process.env.PORT || 3000
 const PORT = 80
 const multer = require("multer")
 const upload = multer()
-
+const axios = require("axios")
 const x = require("./x")
 
+
+const db = axios.create({
+  timeout: 1000,
+  headers: {'X-Custom-Header': 'foobar'},
+  httpAgent: new http.Agent({ keepAlive: true }),
+});
+
+// ##############################
 const show_data = function(req, res, next){
   console.log("##############################")
   x.show()
@@ -14,11 +23,12 @@ const show_data = function(req, res, next){
   next()
 }
 
-
+// ##############################
 app.get('/', (req, res) => {
   res.send('API')
 })
 
+// ##############################
 app.post( "/users", upload.none(), show_data, (req, res) => {
   // res.send()
   // const user_name = req.body.user_name
@@ -29,6 +39,20 @@ app.post( "/users", upload.none(), show_data, (req, res) => {
     }, 10)
 })
 
+// ##############################
+app.get("/users/:id", (req, res) => {
+  q = {"query":"FOR user IN users RETURN user"}
+  db.post('http://arangodb:8529/_api/cursor', q)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });  
+  res.send("x")
+})
+
+// ##############################
 app.listen(80, () => {
   console.log(`Server running on port ${PORT}`)
 })
